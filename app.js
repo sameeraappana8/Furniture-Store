b189da80bdacbc637ac06f1067baddf9d17cdb37
Sameera Appana <appanasa@Admins-MBP.lan1>
2020-06-14 19:47:45 -0400

    added dollar sign in cart

--- app.js
+++ app.js
@@ -120,7 +120,7 @@ class UI {
     </div>
     <div>
       <i class="fas fa-chevron-up" data-id=${item.id}></i>
-      <p class="item-amount">${item.amount}</p>
+      <p class="item-amount">$${item.amount}</p>
       <i class="fas fa-chevron-down" data-id=${item.id}></i>
     </div>`;
     cartContent.appendChild(div);
